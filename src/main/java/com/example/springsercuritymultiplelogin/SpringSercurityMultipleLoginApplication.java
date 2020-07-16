882d87b3d014d75ba094971937f086f2e0f7cb22
Catalina <catalina@Catalinas-Mac.local>
2020-07-16 10:04:30 +0700

    spring sercurity multiple login

---
+++ src/main/java/com/example/springsercuritymultiplelogin/SpringSercurityMultipleLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsercuritymultiplelogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSercurityMultipleLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSercurityMultipleLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springsercuritymultiplelogin/SpringSercurityMultipleLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsercuritymultiplelogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSercurityMultipleLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
